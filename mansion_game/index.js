c7b47e08bb0a58802cb6dcb10265617ea851cabb
Mariekejd <146730379+Mariekejd@users.noreply.github.com>
2024-06-05 11:58:52 +0200

    Add files via upload

---
+++ mansion_game/index.js
@@ -0,0 +1,113 @@
+console.log("hello mansion");
+
+// Selecteer de knoppen en het muntjes element
+const startButton = document.querySelector("#begin");
+const bankButton = document.querySelector("#bank");
+const tafelButton = document.querySelector("#tafel");
+const vloerButton = document.querySelector("#vloer");
+const behangButton = document.querySelector("#behang");
+const werkButton = document.querySelector("#work");
+const audio = new Audio("audio/271141__strange_dragoon__building-placing.mp3");
+const restartButton = document.querySelector("#opnieuw");
+
+const geldMunt = document.querySelector("#muntjes");
+const bericht = document.querySelector("#tekstje");
+
+//bankrekening
+let muntjes = 255;
+
+function veranderBericht() {
+  // Haal de huidige waarde van geldMunt op en zet het om naar een getal
+  if (muntjes >= 500) {
+    bericht.textContent = "Je hebt 500 muntjes aan salaris verdiend!";
+    bericht.style.color = "#109010";
+  } else if (muntjes <= 0) {
+    bericht.textContent =
+      "Oh nee! Je geld is op. Ga werken om bij te verdienen";
+    bericht.style.color = "red";
+  } else {
+    bericht.textContent = "Knap je huis op!";
+    bericht.style.color = "black";
+  }
+}
+
+// Functie om de afbeelding en het geldbedrag te veranderen
+function veranderAfbeelding(afbeelding, kosten) {
+  //bron gebruik parameters: chatgpt, prompt: hoe kan ik parameters gebruiken om dit efficient te maken?
+  let bank = document.querySelector("#bank2"); //basis afbeelding aangeroepen van html
+  bank.src = afbeelding; // Verander de bron van de afbeelding
+  muntjes = muntjes - kosten; //-= is een verkorte versie hiervan
+  geldMunt.textContent = muntjes; // Veranderd de tekst van het muntjes element
+
+  audio.play(); //Speelt audio per klik van een knop
+  veranderBericht(); //Past het bericht aan op basis van nieuwe waarde muntjes
+}
+
+function nieuwGeld() {
+  muntjes = muntjes + 500; // Verhoog de huidige muntjes met 500
+  geldMunt.textContent = muntjes; // Update de tekst van het muntjes element
+  veranderBericht(); //Past het bericht aan op basis van nieuwe waarde muntjes
+  restartButton.style.display = "inline";
+}
+
+function verbergKnoppen() {
+  //Pien heeft me tips gegeven voor de displays van de button
+  //Deze functie verbergt de knoppen bij het starten van de pagina
+  bankButton.style.display = "none";
+  tafelButton.style.display = "none";
+  vloerButton.style.display = "none";
+  behangButton.style.display = "none";
+  werkButton.style.display = "none";
+  startButton.style.display = "inline";
+  restartButton.style.display = "none";
+}
+
+verbergKnoppen();
+
+function toonEersteKnoppen() {
+  bankButton.style.display = "inline";
+  werkButton.style.display = "inline";
+  startButton.style.display = "none";
+
+  bericht.textContent = "Knap je huis op!";
+  bericht.style.color = "black";
+  
+  let houseImage = document.querySelector("#bank2");
+  houseImage.src = "images/mansion.jpg";
+}
+
+restartButton.addEventListener("click", verbergKnoppen);
+startButton.addEventListener("click", toonEersteKnoppen);
+werkButton.addEventListener("click", nieuwGeld);
+
+// Voeg event listeners toe aan de knoppen en roep de functie aan met specifieke argumenten
+bankButton.addEventListener("click", function () {
+  veranderAfbeelding("images/mansion_newcouch.png", 60);
+  bankButton.style.display = "none";
+  tafelButton.style.display = "inline";
+});
+
+tafelButton.addEventListener("click", function () {
+  veranderAfbeelding("images/newtable.png", 100);
+  tafelButton.style.display = "none";
+  vloerButton.style.display = "inline";
+});
+
+vloerButton.addEventListener("click", function () {
+  veranderAfbeelding("images/new_floor.png", 15);
+  vloerButton.style.display = "none";
+  behangButton.style.display = "inline";
+});
+
+behangButton.addEventListener("click", function () {
+  veranderAfbeelding("images/new_wallpaper.png", 80);
+  behangButton.style.display = "none";
+});
+
+//muntjes is a number
+// "" + muntjes is een string
+// muntjes.toString() ook een string
+// muntjesInTekst = muntjes.toString() ?
+
+geldMunt.textContent = muntjes;
+//geldMunt.textContent = muntjes.toString(); --> Walter neergezet
